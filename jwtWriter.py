027d800c61045122fa3f12330265f39bfe1fbace
Erik <erik@Knuts-MBP.home>
2024-03-31 14:50:59 +0200

    Add code so that write_jwt() writes a JWT from scratch, encodes using bas64URL and signs using HMAC SHA25

--- jwtWriter.py
+++ jwtWriter.py
@@ -1,15 +1,32 @@
 import json
+import hmac
+import hashlib
+import base64
+# import pyjwt
+#
+CONST_KEY = "Secret key".encode()
 
 def write_jwt():
 	#Header goes here
-	header = "Header"
+	header = json.dumps({															#JSON object in string format{
+		"alg": "HS256",
+		"typ": "JWT"
+		})
+	uHeader = base64.urlsafe_b64encode(header.encode())								#Byte object (URL-safe)
 
 	#Payload goes here
-	payload = "Payload"
+	payload = json.dumps({															#JSON object in string format{
+		"sub": "1234567890",
+		"name": "John Doe",
+		"iat": 1516239022
+	})
+	uPayload = base64.urlsafe_b64encode(payload.encode())							#Byte object (URL-safe)
 
 	#Signature goes here
-	signature = "Signature"
+	message = uHeader.decode() + "." + uPayload.decode()							#String
+	signature = hmac.new( CONST_KEY, message.encode(), hashlib.sha256).hexdigest()		#Byte object
+	#All three parts to be encode to URL
+	print ( header + "." + payload + "." + signature )
+	return( header + '.' + payload + '.' + signature )
 
-	#All three parts to be encoded to binary
-
-	return(header + '.' + payload + '.' + signature)
+write_jwt()
